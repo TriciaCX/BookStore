7698e683ab9cd48d6fb46fed8d041a955c6c860e
TriciaCX <tricia_chen@foxmail.com>
2019-03-06 20:41:42 +0800

    更新版本1

--- BookStoreV1/bookadmin/src/bookadminv1/Datas.java
+++ BookStoreV1/bookadmin/src/bookadminv1/Datas.java
@@ -9,4 +9,48 @@ public class Datas
 {
 	//用来保存所有图书信息的静态数组，相当于是图书仓库
 		static Book[] BookStore = new Book[999];
+    //使用静态块增加默认书籍信息（方便测试）
+		static {
+			Book book1 = new Book();
+			book1.setBookId("0001");
+			book1.setBookName("红楼梦");
+			book1.setIsbn("000-001");
+			book1.setCount(20);
+			book1.setPrice(100);
+			BookStore[0]=book1;
+			
+			Book book2 = new Book();
+			book2.setBookId("0002");
+			book2.setBookName("水浒传");
+			book2.setIsbn("000-002");
+			book2.setCount(10);
+			book2.setPrice(88);
+			BookStore[1]=book2;
+			
+			Book book3 = new Book();
+			book3.setBookId("0003");
+			book3.setBookName("西游记");
+			book3.setIsbn("000-003");
+			book3.setCount(20);
+			book3.setPrice(70);
+			BookStore[2]=book3;
+			
+			Book book4 = new Book();
+			book4.setBookId("0004");
+			book4.setBookName("三国演义");
+			book4.setIsbn("000-004");
+			book4.setCount(50);
+			book4.setPrice(59);
+			BookStore[3]=book4;
+			
+			Book book5 = new Book();
+			book5.setBookId("0005");
+			book5.setBookName("指环王");
+			book5.setIsbn("000-005");
+			book5.setCount(20);
+			book5.setPrice(100);
+			BookStore[4]=book5;
+			
+			//new BookView().showBooks(BookStore);
+		}
 }
--- BookStoreV1/bookadmin/src/bookadminv1/Main.java
+++ BookStoreV1/bookadmin/src/bookadminv1/Main.java
@@ -8,15 +8,15 @@ public class Main
 		new BookView();
 	}
 	
-	public static void show() {
-		BookBiz bookbiz= new BookBiz();
-		if(bookbiz.getBookCount()<0) {
-			System.out.println("²Ö¿âÒÑ¾­¿ÕÀ²");
-			return;
-		}
-		for(int i=0;i<new BookBiz().getBookCount();i++) {
-			System.out.println(Datas.BookStore[i].getBookId()+"\t"+Datas.BookStore[i].getBookName()+"\t"+Datas.BookStore[i].getCount());
-		}
-	}
+//	public static void show() {
+//		BookBiz bookbiz= new BookBiz();
+//		if(bookbiz.getBookCount()<0) {
+//			System.out.println("²Ö¿âÒÑ¾­¿ÕÀ²");
+//			return;
+//		}
+//		for(int i=0;i<new BookBiz().getBookCount();i++) {
+//			System.out.println(Datas.BookStore[i].getBookId()+"\t"+Datas.BookStore[i].getBookName()+"\t"+Datas.BookStore[i].getCount());
+//		}
+//	}
 
 }
